2d5a70fd186af5d31eb6346895e1cdddc949b63d
miyamotohk <miyamotohk@gmail.com>
2017-05-22 17:59:27 -0300

    projeto3 atualizado

--- projeto3/projeto3.ino
+++ projeto3/projeto3.ino
@@ -1,24 +1,48 @@
+#include "TimerOne.h"
+
+#define pinoSensor 2
 #define pinoMotor 3
-#define pinoSensor 4
+
+const float alpha = 0.001;
+long int contador = 0;
+float vel_atual = 0;
+float vel_desejada = 3000;
+float vel_max = 6000;
+uint8_t pwm = 127;
+
+void RSI_sensor(){
+  contador++; //Incrementa o contador cada vez que a pa passa na frente do sensor
+}
+
+void RSI_timer(){
+  vel_atual = (contador / 0.05) * 30; //Calcula velocidade em RPM
+  contador = 0; //Zera o contador
+}
 
 void setup() {
+  //Configura portas como entrada/saida
   pinMode(pinoMotor, OUTPUT);
   pinMode(pinoSensor, INPUT);
+  
+  //Configura interrupcao por sinal
+  attachInterrupt(0, RSI_sensor, RISING);
+  //Configura interrupcao periodica com T=0.05s
+  Timer1.initialize(50000);
+  Timer1.attachInterrupt(RSI_timer);
+  
   Serial.begin(9600);
 }
 
 void loop() {
-  //teste dos motores
-  /*for(int i=0; i<255; i++){
-    analogWrite(pinoMotor,i);
-    delay(10);
-  }
-  for(int i=255; i>0; i--){
-    analogWrite(pinoMotor,i);
-    delay(10);
-  }*/
-  if(digitalRead(pinoSensor))
-    Serial.println("HIGH");
+  float erro = vel_atual - vel_desejada;
+  int8_t ajuste = (int8_t)(alpha*erro);
+  if((int16_t) (pwm - ajuste) > 255)
+    pwm = 255;
+  else if((int16_t) (pwm - ajuste) < 0)
+    pwm = 0;
   else
-    Serial.println("LOW");
+    pwm = pwm - ajuste;
+    
+  analogWrite(pinoMotor,pwm);
+  Serial.println(vel_atual);
 }
