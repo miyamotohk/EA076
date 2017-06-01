1f62b4347284e52bfe848f7005e2df4152684324
miyamotohk <miyamotohk@gmail.com>
2017-06-01 00:27:25 -0300

    atualizacao nos comentarios

--- projeto3/projeto3.ino
+++ projeto3/projeto3.ino
@@ -3,17 +3,20 @@
 #define pinoSensor 2
 #define pinoMotor 3
 
-const float alpha = 0.001;
-long int contador = 0;
-float vel_atual = 0;
-float vel_desejada = 3000;
-float vel_max = 6000;
-uint8_t pwm = 127;
+//Variaveis globais para controle de processos
+const float alpha = 0.0007;   //Parametro do controle proporcional (obtido empiricamente)
+long int contador = 0;       //Contador de passagens do obstaculo pelo sensor
+float vel_atual = 0;         //Velocidade atual do motor (calculada em RSI_sensor)
+float vel_desejada = 3000;   //Velocidade desejada para o motor (deve ser ajustada manualmente no codigo)
+float vel_max = 6000;        //Velocidade maxima do motor (estimada a partir das suas especificacoes)
+uint8_t pwm = 127;           //Valor enviado para o motor na primeira iteracao do controle
 
+//RSI acionada pela detecao de um obstaculo pelo sensor
 void RSI_sensor(){
   contador++; //Incrementa o contador cada vez que a pa passa na frente do sensor
 }
 
+//RSI periodica para calculo da velocidade
 void RSI_timer(){
   vel_atual = (contador / 0.05) * 30; //Calcula velocidade em RPM
   contador = 0; //Zera o contador
@@ -30,19 +33,23 @@ void setup() {
   Timer1.initialize(50000);
   Timer1.attachInterrupt(RSI_timer);
   
+  //Inicializa comuincacao serial
   Serial.begin(9600);
 }
 
 void loop() {
-  float erro = vel_atual - vel_desejada;
-  int8_t ajuste = (int8_t)(alpha*erro);
+  //Rotina de controle proporcional do motor
+  float erro = vel_atual - vel_desejada; //Calcula erro
+  int8_t ajuste = (int8_t)(alpha*erro); //Calcula ajuste
+  
+  //Calucla valor valor ajustado (pwm - ajuste), que deve estar entre 0 e 255
   if((int16_t) (pwm - ajuste) > 255)
-    pwm = 255;
+    pwm = 255; //Se for maior que 255, satura em 255
   else if((int16_t) (pwm - ajuste) < 0)
-    pwm = 0;
+    pwm = 0; //Se for menor que 0, satura em 0
   else
     pwm = pwm - ajuste;
-    
-  analogWrite(pinoMotor,pwm);
-  Serial.println(vel_atual);
+  
+  analogWrite(pinoMotor,pwm); //Envia valor pwm para o motor
+  Serial.println(vel_atual); //Escreve velocidade atual no monitor serial para acompanhamento
 }
